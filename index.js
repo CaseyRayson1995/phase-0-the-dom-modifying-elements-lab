9df6306ab2f5c6ae54b56d331e2d32740fca9a68
CaseyRayson1995 <caseyrayson1995@gmail.com>
2024-09-12 23:18:12 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// Creating a new <h1> element
+const newHeader = document.createElement('h1');
+
+// the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.id = 'victory';
+
+// the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text 
+//"YOUR-NAME is the champion" (with your name in place of YOUR-NAME) inside it
+newHeader.textContent = "Casey is the champion";
